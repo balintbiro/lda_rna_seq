e2b8e0ba1ac6dc3d7dc11fbafb224f584842d949
Balint Biro <biro.balint.szie@gmail.com>
2022-03-21 15:54:29 +0100

    index file

---
+++ code/4_prepare_index_file.bash
@@ -0,0 +1,5 @@
+#create directory for index files
+mkdir ../data/idx/
+
+#prepare index files
+hisat2-build ../data/Bos_taurus.ARS-UCD1.2.dna.toplevel.fa ../data/idx/Bos_taurus.ARS-UCD1.2.dna.toplevel.hisat

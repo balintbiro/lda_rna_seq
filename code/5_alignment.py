925bc21650b4abb77ebbe0576c3fa1151c4f7396
Balint Biro <biro.balint.szie@gmail.com>
2022-03-21 18:07:49 +0100

    alignment

---
+++ code/5_alignment.py
@@ -0,0 +1,17 @@
+#import the required modules
+import os
+import pandas as pd
+from subprocess import call
+
+#create directory for alignments
+if.os.path.exists('../data/alignments/')==False:
+	os.mkdir('../data/alignments/')
+
+#define function for alignment
+def alignment(filename):
+	file_id=filename.split('.')[0]
+	alignment_filename=f'{file_id}.sam'
+	call(f'hisat2 -x ../data/idx/Bos_taurus.ARS-UCD1.2.dna.toplevel.hisat -U ../data/fastas/{filename} -S ../data/alignments/{alignment_filename}')
+
+#make the function work
+pd.Series(os.listdir('../data/')).apply(alignment)

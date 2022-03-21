545a761ece35df7eb8c9a3836dac070e6336c02d
Balint Biro <biro.balint.szie@gmail.com>
2022-03-21 11:50:08 +0100

    download reference files

---
+++ code/get_reference_files.bash
@@ -0,0 +1,5 @@
+#get the genome file
+wget --directory-prefix=../data/ http://ftp.ensembl.org/pub/release-105/fasta/bos_taurus/dna/Bos_taurus.ARS-UCD1.2.dna.toplevel.fa.gz
+
+#get index file
+wget --directory-prefix=../data/ http://ftp.ensembl.org/pub/release-105/gtf/bos_taurus/Bos_taurus.ARS-UCD1.2.105.gtf.gz

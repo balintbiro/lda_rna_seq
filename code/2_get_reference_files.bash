b07db84cbf15b279b9e5c3822e534cffd8ac8801
Balint Biro <biro.balint.szie@gmail.com>
2022-03-21 15:06:25 +0100

    gunzip reference genome

--- code/2_get_reference_files.bash
+++ code/2_get_reference_files.bash
@@ -1,5 +1,6 @@
-#get the genome file
+#get the genome file and gunzip it
 wget --directory-prefix=../data/ http://ftp.ensembl.org/pub/release-105/fasta/bos_taurus/dna/Bos_taurus.ARS-UCD1.2.dna.toplevel.fa.gz
+gunzip -d ../data/Bos_taurus.ARS-UCD1.2.dna.toplevel.fa.gz ../data/Bos_taurus.ARS-UCD1.2.dna.toplevel.fa
 
 #get index file
 wget --directory-prefix=../data/ http://ftp.ensembl.org/pub/release-105/gtf/bos_taurus/Bos_taurus.ARS-UCD1.2.105.gtf.gz

2105481c7c5312330cbf8aa954e38ac6302b7d8b
Balint Biro <biro.balint.szie@gmail.com>
2022-03-21 11:36:03 +0100

    setting up the environment

---
+++ code/setting_up_environment.bash
@@ -0,0 +1,12 @@
+#create and activate the environment
+conda create --name rna_seq_env
+conda activate rna_seq_env
+
+#make the required directories
+mkdir code
+mkdir data
+mkdir results
+
+#install the programs
+conda install -c bioconda hisat2
+conda install -c bioconda htseq

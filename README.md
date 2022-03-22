705126cecc8a4fe5dc869fd2fcefd8f3d370106f
Balint Biro <biro.balint.szie@gmail.com>
2022-03-22 17:06:19 +0100

    correcting typo and add EMBL link

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # lda_rna_seq
  RNA-seq analysis in Left-sided Displacement of the Abomasum (LDA) affected cows.
+
+https://www.ebi.ac.uk/training/online/courses/functional-genomics-ii-common-technologies-and-data-analysis-methods/rna-sequencing/performing-a-rna-seq-experiment/
--- code/6_gene_counts.py
+++ code/6_gene_counts.py
@@ -5,7 +5,7 @@ from subprocess import call
 
 #define function for counting genes
 def gene_counts(sam_filename):
-	count_filename=f'{sam_filename.split('.')[0]}.txt'
+	count_filename=f"{sam_filename.split('.')[0]}.txt"
 	call(f'htseq-count ../data/alignments/{sam_filename} ../data/Bos_taurus.ARS-UCD1.2.105.gtf > ../data/gene_counts/{count_filename}',
 		shell=True)
 

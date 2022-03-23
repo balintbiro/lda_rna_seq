4d5e9cbf0b5ec6b91605239c9bc211e8f88a1e3d
Balint Biro <biro.balint.szie@gmail.com>
2022-03-23 11:23:10 +0100

    gunzip .gtf file

--- code/2_get_reference_files.bash
+++ code/2_get_reference_files.bash
@@ -4,3 +4,4 @@ gunzip -d ../data/Bos_taurus.ARS-UCD1.2.dna.toplevel.fa.gz ../data/Bos_taurus.AR
 
 #get index file
 wget --directory-prefix=../data/ http://ftp.ensembl.org/pub/release-105/gtf/bos_taurus/Bos_taurus.ARS-UCD1.2.105.gtf.gz
+gunzip -d ../data/Bos_taurus.ARS-UCD1.2.105.gtf.gz ../data/Bos_taurus.ARS-UCD1.2.105.gtf

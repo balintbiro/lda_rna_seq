fe5dc40e3dcabd499e9a4acadaeb478339c65b7d
Balint Biro <biro.balint.szie@gmail.com>
2022-03-23 11:16:52 +0100

    alignment

---
+++ code/5_alignment.bash
@@ -0,0 +1,13 @@
+#create directory for alignments
+mkdir ../data/alignments/
+
+#align the reads to the reference genome - readable form
+for file in ../data/fastas/*
+do
+	filename_without_path=$(basename -- "$file")
+	alignment_filename="${filename_without_path%%.*}"
+	hisat2 -x ../data/idx/Bos_taurus.ARS-UCD1.2.dna.toplevel.hisat -U "$file" -S "../data/alignments/$alignment_filename.sam"
+done
+
+#using nohup
+nohup sh -c 'for file in ../data/fastas/*;do filename_without_path=$(basename -- "$file"); alignment_filename="${filename_without_path%%.*}"; hisat2 -x ../data/idx/Bos_taurus.ARS-UCD1.2.dna.toplevel.hisat -U "$file" -S "../data/alignments/$alignment_filename.sam"; done'

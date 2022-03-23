bd459e5f26fea3b58425062c8c78ae1411012aea
Balint Biro <biro.balint.szie@gmail.com>
2022-03-23 13:59:55 +0100

    correct variable

--- code/6_gene_counts.bash
+++ code/6_gene_counts.bash
@@ -10,4 +10,4 @@ do
 done
 
 #using nohup
-nohup sh -c 'for file in ../data/alignments/*; do filename_without_path=$(basename -- $"file"); gene_count_filename="${filename_without_path%.*}"; htseq-count "$file" ../data/Bos_taurus.ARS-UCD1.2.105.gtf > "../data/gene_counts/$gene_count_filename.txt"; done' &
+nohup sh -c 'for file in ../data/alignments/*; do filename_without_path=$(basename -- "$file"); gene_count_filename="${filename_without_path%.*}"; htseq-count "$file" ../data/Bos_taurus.ARS-UCD1.2.105.gtf > "../data/gene_counts/$gene_count_filename.txt"; done' &
